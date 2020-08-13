8831c83b9867deaf214479755cdbf9c06176d97c
dev-abhaysingh <48999616+dev-abhaysingh@users.noreply.github.com>
2020-08-13 22:09:02 +0530

    Updates

---
+++ precache-manifest.7907b25873dd8745b304225b5a961266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d699602df658a21d79c4b50d27eca8b",
+    "url": "/recipe-react/index.html"
+  },
+  {
+    "revision": "46a7e59d6f093a10eef6",
+    "url": "/recipe-react/static/css/main.fcc27dbb.chunk.css"
+  },
+  {
+    "revision": "6bace0ef3250eeae4919",
+    "url": "/recipe-react/static/js/2.2bfb2508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-react/static/js/2.2bfb2508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a7e59d6f093a10eef6",
+    "url": "/recipe-react/static/js/main.77a1cd95.chunk.js"
+  },
+  {
+    "revision": "e509b3e64eb500bc5e37",
+    "url": "/recipe-react/static/js/runtime-main.58a17e23.js"
+  }
+]);
